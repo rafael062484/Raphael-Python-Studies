8699f4d24494a5a581f88758e8bceda6e95f1311
Raphael Chai <rl290218@gmail.com>
2024-10-03 22:51:07 +0300

    Add files via upload

---
+++ Average_calculation.py
@@ -0,0 +1,9 @@
+grades = [81, 70, 75, 96, 99, 100, 87, 80, 100]
+Score_summary = sum(grades)
+print(Score_summary)
+length_grades = len(grades)
+print(length_grades)
+average_score = Score_summary / length_grades
+print(int(average_score))
+a = str("If there is trut, if there is no falsa ! ")
+print(average_score in grades,str(a))
